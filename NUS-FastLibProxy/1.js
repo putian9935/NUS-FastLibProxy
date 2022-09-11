04032243b0d11a7ca0b4398644ace97370617db9
putian9935 <64566589+putian9935@users.noreply.github.com>
2022-09-11 11:01:18 +0800

    fix typo

--- NUS-FastLibProxy/1.js
+++ NUS-FastLibProxy/1.js
@@ -1,4 +1,4 @@
 document.querySelector("select").value = "NUSSTU"; // Change correspondingly if you're in other domains
-document.forms[1].user.value ="USERNAME; // Change USERNAME to your studentID, e1234567, e.g.
+document.forms[1].user.value ="USERNAME"; // Change USERNAME to your studentID, e1234567, e.g.
 document.forms[1].pass.value ="PASSWORD"; // Change PASSWORD to your password for login
 document.forms[1].submit();
