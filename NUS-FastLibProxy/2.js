e8daed34ede12bf2e711c880264e23b357243917
putian9935 <64566589+putian9935@users.noreply.github.com>
2022-09-11 11:00:30 +0800

    shift to manifest V3

---
+++ NUS-FastLibProxy/2.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName("input")[1].click();
--- NUS-FastLibProxy/background.js
+++ NUS-FastLibProxy/background.js
@@ -1,17 +1,21 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
         if(tab.url.substr(0, "https://proxylogin.nus.edu.sg/libproxy1/public/login.asp".length) == "https://proxylogin.nus.edu.sg/libproxy1/public/login.asp") {
-            // Change correspondingly if you're in other domains
-            chrome.tabs.executeScript(tabId, {code : 'document.querySelector("select").value = "NUSSTU"; '});
-            // Change USERNAME to your studentID, e1234567, e.g.
-            chrome.tabs.executeScript(tabId, {code : 'document.forms[1].user.value ="USERNAME";'});
-            // Change PASSWORD to your password yo login
-            chrome.tabs.executeScript(tabId, {code : 'document.forms[1].pass.value ="PASSWORD";'});
-            chrome.tabs.executeScript(tabId, {code : 'document.forms[1].submit();'});        
+            chrome.scripting.executeScript(
+                {
+                    target:{tabId : tabId},
+                    files : ['1.js']
+                }
+            );   
         }
 
         if(tab.url.substr(0, "https://proxylogin.nus.edu.sg/libproxy1/public/aup.asp".length) == "https://proxylogin.nus.edu.sg/libproxy1/public/aup.asp") {
-            chrome.tabs.executeScript(tabId, {code : 'document.getElementsByTagName("input")[1].click();'});
+            chrome.scripting.executeScript(
+                {
+                    target:{tabId : tabId},
+                    files : ['2.js']
+                }
+            );
         }
     }
-  );
+);
